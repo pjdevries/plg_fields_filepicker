bc2da7765a4273fa9dd80b8c2214bfe6dcb5b62d
Pieter-Jan de Vries <pieter@obix.nl>
2021-08-16 17:51:03 +0200

    Adds removal of empty folders to script uninstall.

--- plugins/fields/filepicker/script.php
+++ plugins/fields/filepicker/script.php
@@ -8,20 +8,85 @@ defined('_JEXEC') or die;
 
 class plgfieldsfilepickerInstallerScript
 {
+
 	public function postflight($route, PluginAdapter $adapter)
 	{
-        // Enable plugin on first installation only.
-        if ($route === 'install')
-        {
-            $db = Factory::getDbo();
-            $query = sprintf('UPDATE %s SET %s = 1 WHERE %s = %s AND %s = %s',
-                $db->quoteName('#__extensions'),
-                $db->quoteName('enabled'),
-                $db->quoteName('type'), $db->quote('plugin'),
-	            $db->quoteName('name'), $db->quote('PLG_FIELDS_FILEPICKER')
-            );
-            $db->setQuery($query);
-            $db->execute();
-        }
-    }
+		// Enable plugin on first installation only.
+		switch ($route)
+		{
+		case 'install':
+			$this->activatePlugin();
+			break;
+
+		case 'uninstall':
+			$this->removeEmptyFolders();
+			break;
+		}
+	}
+
+	private function activatePlugin()
+	{
+		$db    = Factory::getDbo();
+		$query = sprintf(
+			'UPDATE %s SET %s = 1 WHERE %s = %s AND %s = %s',
+			$db->quoteName('#__extensions'),
+			$db->quoteName('enabled'),
+			$db->quoteName('type'), $db->quote('plugin'),
+			$db->quoteName('name'), $db->quote('PLG_FIELDS_FILEPICKER')
+		);
+		$db->setQuery($query);
+		$db->execute();
+	}
+
+	private function removeEmptyFolders(): void
+	{
+		$folderPaths = [
+			'layouts'   => [
+				'layouts/obix/form/field',
+			],
+			'libraries' => [
+				'libraries/Obix/Filesystem/Folder/Scanner/Acceptor',
+				'libraries/Obix/Filesystem/Folder/Scanner',
+				'libraries/Obix/Filesystem/Folder',
+				'libraries/Obix/Form/Field',
+			],
+			'media'     => [
+				'media/plg_fields_filepicker/css',
+				'media/plg_fields_filepicker/images',
+				'media/plg_fields_filepicker/js',
+			],
+		];
+
+		foreach ($folderPaths as $base => $paths)
+		{
+			foreach ($paths as $path)
+			{
+				$this->removeEmptyFolder(JPATH_ROOT . '/' . $base, JPATH_ROOT . '/' . $path);
+			}
+		}
+	}
+
+	private function removeEmptyFolder(string $base, string $folderPath): void
+	{
+		do
+		{
+			if (!($this->isEmptyFolder($folderPath) && @rmdir($folderPath)))
+			{
+				break;
+			}
+
+			$folderPath = dirname($folderPath);
+		} while ($folderPath > $base);
+	}
+
+	private function isEmptyFolder(string $folderPath): bool
+	{
+		if (!(is_dir($folderPath) && is_readable($folderPath)))
+		{
+			return false;
+		}
+
+		return !(new \FilesystemIterator($folderPath))->valid();
+	}
+
 }
